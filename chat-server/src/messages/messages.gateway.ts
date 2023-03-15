ac775b3d611b8b3318beeff691c464b0471dd48c
Muhammed Ali <madilekci@gmail.com>
2023-03-15 17:40:01 +0300

    Add cors origin

--- chat-server/src/messages/messages.gateway.ts
+++ chat-server/src/messages/messages.gateway.ts
@@ -3,7 +3,11 @@ import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  }
+})
 export class MessagesGateway {
   constructor(private readonly messagesService: MessagesService) {}
 
